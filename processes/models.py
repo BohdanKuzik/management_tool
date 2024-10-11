43edd25c7be9895ecfd78c771e27d817fce24777
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-10-12 01:12:23 +0200

    edited file requirements.txt and added model Snapshot

--- processes/models.py
+++ processes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Snapshot(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    process_data = models.JSONField()
+
+    def __str__(self):
+        return f"Snapshot by {self.author} at {self.timestamp}"
