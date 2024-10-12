0f30c133552e9b1527260ee0ae22789d3cad360c
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-10-12 18:00:36 +0200

    added filter and button take snapshot

--- processes/urls.py
+++ processes/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 
-from processes.views import index
 from processes.views import (
-    process_list,
-    process_list_partial,
-    kill_process,
+    index,
+    ProcessListView,
+    ProcessListPartialView,
+    KillProcessView,
+    take_snapshot,
 )
 
 
 urlpatterns = [
     path("", index, name="index"),
-    path("processes/", process_list, name="process_list"),
-    path('processes/partial/', process_list_partial, name='process_list_partial'),
-    path('processes/kill/<int:pid>/', kill_process, name='kill_process'),
+    path("processes/", ProcessListView.as_view(), name="process_list"),
+    path("processes/partial/", ProcessListPartialView.as_view(), name="process_list_partial"),
+    path("processes/kill/<int:pid>/", KillProcessView.as_view(), name="kill_process"),
+    path("processes/take_snapshot/", take_snapshot, name="take_snapshot"),
 ]
 
 app_name = "processes"
--- processes/views.py
+++ processes/views.py
@@ -1,34 +1,37 @@
+import json
+
+from django.utils import timezone
+from django.shortcuts import redirect
 import psutil
 import logging
-from django.utils import timezone
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 
+from django.views import View
+from django.views.generic import TemplateView
+
+from processes.models import Snapshot
 
 logger = logging.getLogger(__name__)
 
 
-def index(request):
-    num_four = 2 + 2
-
-    context = {
-        "num_four": num_four,
-    }
-
-    return render(request, "processes/index.html", context=context)
+def index():
+    pass
 
 
-def get_processes():
+def get_processes(pid_filter=None, status_filter=None, name_filter=None):
     processes = []
     for proc in psutil.process_iter(["pid", "status", "name"]):
         try:
+            if (pid_filter and str(proc.pid) != pid_filter) or \
+               (status_filter and proc.status() != status_filter) or \
+               (name_filter and name_filter.lower() not in proc.name().lower()):
+                continue
+
             processes.append({
                 "pid": proc.pid,
                 "status": proc.status(),
-                "start_time": timezone.datetime.fromtimestamp(proc.create_time()),
+                "start_time": timezone.datetime.fromtimestamp(proc.create_time()).strftime('%Y-%m-%d %H:%M:%S'),
                 "name": proc.name(),
-                "memory_usage": proc.memory_info().rss / (1024 * 1024),
+                "memory_usage": round(proc.memory_info().rss / (1024 * 1024), 4),
                 "cpu_usage": proc.cpu_percent(interval=0),
             })
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
@@ -36,21 +39,55 @@ def get_processes():
     return processes
 
 
-def process_list(request):
-    processes = get_processes()
-    return render(request, "processes/process_list.html", {"processes": processes})
+class ProcessListView(TemplateView):
+    template_name = 'processes/process_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pid_filter = self.request.GET.get("pid")
+        status_filter = self.request.GET.get("status")
+        name_filter = self.request.GET.get("name")
+
+        processes = get_processes(pid_filter, status_filter, name_filter)
+        context['processes'] = processes
+        return context
 
 
-def process_list_partial(request):
-    processes = get_processes()
-    return render(request, "processes/process_table.html", {"processes": processes})
+class ProcessListPartialView(TemplateView):
+    template_name = 'processes/process_table.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pid_filter = self.request.GET.get("pid")
+        status_filter = self.request.GET.get("status")
+        name_filter = self.request.GET.get("name")
+
+        processes = get_processes(pid_filter, status_filter, name_filter)
+        context['processes'] = processes
+        return context
 
 
-def kill_process(request, pid):
-    if request.method == "POST":
+class KillProcessView(View):
+    def post(self, request, pid, *args, **kwargs):
         try:
             p = psutil.Process(pid)
             p.terminate()
         except psutil.NoSuchProcess:
-            pass
-    return HttpResponseRedirect(reverse("processes:process_list"))
+            logger.warning(f"Process {pid} not found.")
+        except psutil.AccessDenied:
+            logger.error(f"Access denied when trying to terminate process {pid}.")
+        return redirect("processes:process_list")
+
+
+def take_snapshot(request):
+    if request.method == "POST":
+        processes = get_processes()
+        snapshot_data = json.dumps(processes)
+
+        Snapshot.objects.create(
+            author=request.user,
+            process_data=snapshot_data,
+            timestamp=timezone.now()
+        )
+
+        return redirect("processes:process_list")
