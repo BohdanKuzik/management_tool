89e92d9432eb87ee47669999c7e58589bf008071
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-10-12 13:32:45 +0200

    added list with data and added file style.css

--- processes/views.py
+++ processes/views.py
@@ -1,3 +1,39 @@
+import psutil
+import logging
+from django.utils import timezone
 from django.shortcuts import render
 
-# Create your views here.
+
+logger = logging.getLogger(__name__)
+
+
+def index(request):
+    num_four = 2 + 2
+
+    context = {
+        "num_four": num_four,
+    }
+
+    return render(request, "processes/index.html", context=context)
+
+
+def get_processes():
+    processes = []
+    for proc in psutil.process_iter(["pid", "status", "name"]):
+        try:
+            processes.append({
+                "pid": proc.pid,
+                "status": proc.status(),
+                "start_time": timezone.datetime.fromtimestamp(proc.create_time()),
+                "name": proc.name(),
+                "memory_usage": proc.memory_info().rss / (1024 * 1024),
+                "cpu_usage": proc.cpu_percent(interval=None),
+            })
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
+            logger.warning(f"Error while accessing process: {e}")
+    return processes
+
+
+def process_list(request):
+    processes = get_processes()
+    return render(request, "processes/process_list.html", {"processes": processes})
