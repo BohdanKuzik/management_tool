ffba6a787ba0fa11cef69a62e360a2d694ac7282
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-10-13 15:07:48 +0200

    changes in views added logic of authentication

--- processes/urls.py
+++ processes/urls.py
@@ -6,6 +6,8 @@ from processes.views import (
     ProcessListPartialView,
     KillProcessView,
     take_snapshot,
+    RegisterUser,
+    LoginUser,
 )
 
 
@@ -15,6 +17,8 @@ urlpatterns = [
     path("processes/partial/", ProcessListPartialView.as_view(), name="process_list_partial"),
     path("processes/kill/<int:pid>/", KillProcessView.as_view(), name="kill_process"),
     path("processes/take_snapshot/", take_snapshot, name="take_snapshot"),
+    path("login/", LoginUser.as_view(), name="login"),
+    path("register/", RegisterUser.as_view(), name="register")
 ]
 
 app_name = "processes"
--- processes/views.py
+++ processes/views.py
@@ -1,26 +1,36 @@
 import json
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.utils import timezone
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 import psutil
 import logging
 
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.views.generic.base import ContextMixin
 
+from processes.forms import RegisterUserForm
 from processes.models import Snapshot
 
 logger = logging.getLogger(__name__)
 
 
-def index():
-    pass
+def index(request):
+    return render(request, 'processes/process_list.html')
 
 
 def get_processes(pid_filter=None, status_filter=None, name_filter=None):
     processes = []
     for proc in psutil.process_iter(["pid", "status", "name"]):
         try:
+
+            if proc.pid == 0:
+                continue
+
             if (pid_filter and str(proc.pid) != pid_filter) or \
                (status_filter and proc.status() != status_filter) or \
                (name_filter and name_filter.lower() not in proc.name().lower()):
@@ -29,7 +39,7 @@ def get_processes(pid_filter=None, status_filter=None, name_filter=None):
             processes.append({
                 "pid": proc.pid,
                 "status": proc.status(),
-                "start_time": timezone.datetime.fromtimestamp(proc.create_time()).strftime('%Y-%m-%d %H:%M:%S'),
+                "start_time": timezone.datetime.fromtimestamp(proc.create_time()).strftime("%Y-%m-%d %H:%M:%S"),
                 "name": proc.name(),
                 "memory_usage": round(proc.memory_info().rss / (1024 * 1024), 4),
                 "cpu_usage": proc.cpu_percent(interval=0),
@@ -39,32 +49,27 @@ def get_processes(pid_filter=None, status_filter=None, name_filter=None):
     return processes
 
 
-class ProcessListView(TemplateView):
-    template_name = 'processes/process_list.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+class ProcessListMixin(ContextMixin, View):
+    def get_filtered_processes(self):
         pid_filter = self.request.GET.get("pid")
         status_filter = self.request.GET.get("status")
         name_filter = self.request.GET.get("name")
 
         processes = get_processes(pid_filter, status_filter, name_filter)
-        context['processes'] = processes
-        return context
-
-
-class ProcessListPartialView(TemplateView):
-    template_name = 'processes/process_table.html'
+        return processes
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        pid_filter = self.request.GET.get("pid")
-        status_filter = self.request.GET.get("status")
-        name_filter = self.request.GET.get("name")
-
-        processes = get_processes(pid_filter, status_filter, name_filter)
-        context['processes'] = processes
+        context["processes"] = self.get_filtered_processes()
         return context
+
+
+class ProcessListView(ProcessListMixin, TemplateView):
+    template_name = "processes/process_list.html"
+
+
+class ProcessListPartialView(ProcessListMixin, TemplateView):
+    template_name = "processes/process_table.html"
 
 
 class KillProcessView(View):
@@ -91,3 +96,25 @@ def take_snapshot(request):
         )
 
         return redirect("processes:process_list")
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = "processes/register.html"
+    success_url = reverse_lazy("processes:login")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Sign up"
+        return context
+
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = "processes/login.html"
+    success_url = reverse_lazy("processes:login")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Login"
+        return context
